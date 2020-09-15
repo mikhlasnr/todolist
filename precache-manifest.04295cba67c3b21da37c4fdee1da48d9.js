872538b60c6399cacccd2e60b541b37342b2d931
M Ikhlas N R <minrnullpo@gmail.com>
2020-09-15 23:14:44 +0700

    Updates

---
+++ precache-manifest.04295cba67c3b21da37c4fdee1da48d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87eb201ac8f36cde588f95a3a3f1387",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b2326da00d2848f0ad96",
+    "url": "/my-app/static/css/main.fbde5afc.chunk.css"
+  },
+  {
+    "revision": "871951e34ac4f9c023d0",
+    "url": "/my-app/static/js/2.d7dde4cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.d7dde4cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2326da00d2848f0ad96",
+    "url": "/my-app/static/js/main.af547cef.chunk.js"
+  },
+  {
+    "revision": "d95543f03fee126c68f4",
+    "url": "/my-app/static/js/runtime-main.2ebac08e.js"
+  }
+]);
